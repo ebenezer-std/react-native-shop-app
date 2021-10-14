1f3d791caa5f248f614ff6cd873fcaaac6cb0158
Ebenezer <eaanertey@st.ug.edu.gh>
2021-10-14 08:31:58 +0000

    Initial Commits

---
+++ components/Trial.js
@@ -0,0 +1,66 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+const Trial =()=> {
+  return (
+    <SafeAreaView style={{alignItems:'center', flex: 1}} >
+    <View style={styles.container}>
+      <Text  >
+        Here are some boxes of different colours
+      </Text>
+      <View style={{borderStyle: 'dashed', backgroundColor:'cyan',alignItems:'center'}}>
+
+      <Text >
+        Cyan
+      </Text>
+      </View>
+
+      <View>
+
+      <Text>
+        Cyan
+      </Text>
+      </View>
+      <View>
+
+      <Text>
+        Cyan
+      </Text>
+      </View>
+
+    </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    /* paddingHorizontal: 50,
+    paddingVertical: 10,
+    backgroundColor: "green",
+    marginTop: 15,
+    marginHorizontal: 19,
+    borderRadius: 5,
+    borderColor: "red",
+    borderWidth: 5,
+    borderTopRightRadius: 40,
+    justifyContent: 'center',
+    alignItems: 'center' */
+    // marginHorizontal:1,
+    marginTop:50,
+    // justifyContent: 'center'
+
+
+
+  
+    
+  }
+})
+
+export default App;
+
+
