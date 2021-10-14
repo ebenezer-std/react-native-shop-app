8e0b7a1b46940db6ccd7d72c5ae7b5d8e6bfe9f4
Ebenezer <eaanertey@st.ug.edu.gh>
2021-10-14 23:50:15 +0000

    Added Some UI component and styling

--- App.js
+++ App.js
@@ -1,16 +1,57 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet, SafeAreaView} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './components/Home.js';
+import Login from './components/Login.js';
 
 const App =()=>{
+  
+  const MainNavigator = createStackNavigator()
+  
   return(
-    <View>
+   
+    <View style={{flex:1}}>
+      <NavigationContainer>
+        <MainNavigator.Navigator initialRouteName = "Home">
+          <MainNavigator.Screen name="Login" component={Login} /> 
+          <MainNavigator.Screen name="Home" component={Home} />      
+           </MainNavigator.Navigator>
+      </NavigationContainer>
+      
 
     </View>
+    /* <Text>
+      Hey there welcome home
+    </Text> */
+
+  
 
   );
 }
 
 
 export default App;
+
+// import React from 'react';
+// import {View, StyleSheet, Image,SafeAreaView} from 'react-native';
+// import { FontAwesome, Feather, AntDesign,Ionicons  } from '@expo/vector-icons'; 
+
+
+// export default function Home({component}) {
+//     return (
+//       <SafeAreaView>
+
+//         <View style={{flex: 1}}>
+//            <FontAwesome name="bicycle" size={24} color="black" />
+//            <Feather name="menu" size={24} color="black" />
+//            <AntDesign name="search1" size={24} color="black" />
+//            <Ionicons name="notifications-outline" size={24} color="black" />
+
+
+
+//         </View>
+//       </SafeAreaView>
+//     )
+// }
---
+++ components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+
+
+export default function Login() {
+    return (
+        <View style={{flex: 1}}> 
+            <Text style={{fontWeight: 'bold'}}>
+                Hey Welcome to the login page
+            </Text>
+        </View>
+    )
+}
